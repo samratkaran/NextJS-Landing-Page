8c1dac74dd8435c83a0c508c526284a691d9183d
Karan samrat <karansamrat05039@gmail.com>
2024-02-28 12:26:36 +0530

    few chnages

--- my-app/src/app/coffee/page.tsx
+++ my-app/src/app/coffee/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Coffee (){
   return(<>
     <h2 className='text-3xl text-teal-900 text-center'> this is coffee  page</h2>
-    <h2 className='text-3xl text-teal-900 text-center'> this is coffee 2 page</h2></>
+    
+    </>
   )
 }
