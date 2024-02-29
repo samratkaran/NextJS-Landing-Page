58e24086441ee9d6b8c6160b0b3a6bbf0dbee7c0
Karan samrat <karansamrat05039@gmail.com>
2024-02-29 12:48:51 +0530

    background changes and navbar fix

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
- <main className=" min-h-screen bg-black/[0.96] antialiased  bg-grid-white/[0.02]">
- <h1 className="text-2xl text-center">karan samrat</h1> 
+ <main className="bg-white  antialiased  bg-grid-white/[0.02] min-h-screen">
+
  
  </main>
   );
